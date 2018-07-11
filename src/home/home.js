9711f66b9fb489a37029d007504df35673951e6b
Maarten Wilschut <maarten.wilschut@luminis.eu>
2018-07-11 10:50:34 +0200

    added some devCon text

--- src/home/home.js
+++ src/home/home.js
@@ -4,7 +4,18 @@ class Home extends Component {
     render() {
         return (
             <div>
-                What is DevCon
+                Our developer conference is a platform where you and your colleagues can share knowledge with
+                like-minded IT specialists. We will focus on trends and the latest software development technologies.
+                Multiple technical sessions will be presented by renowned international and national speakers who will
+                also share their experience and knowledge through a series of parallel sessions.
+                <br/><br/>
+                Check out the agenda to build your own conference learning experience. In between the sessions we offer
+                15 minutes breaks so there is some time to chat amongst speakers or fellow attendees, and to grab a cup
+                of coffee or tea. There is no need to rush from one session to the next. The cinema rooms all offer
+                spacious and comfortable seats and a big screen to follow along.
+                <br/><br/>
+                Meet people, ask questions, exchange your experiences and explore the future together. Enjoy this
+                learning experience at Luminis’ DevCon; the developers conference of the year!
             </div>
         );
     }
